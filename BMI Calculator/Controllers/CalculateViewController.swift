15345c42e2ba8ca394356ab55a38c0a10e2b7149
Uma Kumari <umagarhwa3@gmail.com>
2026-02-01 12:38:39 +0530

    first commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightS: UISlider!
+    @IBOutlet weak var weightS: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSlider(_ sender: UISlider) {
+      let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+        
+        }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+       let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightS.value;
+        let weight = weightS.value
+        
+//        let bmi = weight / pow(height, 2)
+        let bmi = weight / (height*height)
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
+}
+
---
+++ BMI Calculator/models/CalculateBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculateBrain.swift
+//  BMI Calculator
+//
+//  Created by uma kumari on 15/01/26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import Foundation
